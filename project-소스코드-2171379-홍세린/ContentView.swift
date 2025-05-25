18635f80525816d0973a576a884c5a4f7a0fe620
tpfls <165572388+tpfls@users.noreply.github.com>
2025-05-25 18:34:04 +0900

    Initial Commit

---
+++ project-소스코드-2171379-홍세린/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project-소스코드-2171379-홍세린
+//
+//  Created by 홍세린 on 5/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project-소스코드-2171379-홍세린/project__________2171379_________App.swift
@@ -0,0 +1,17 @@
+//
+//  project__________2171379_________App.swift
+//  project-소스코드-2171379-홍세린
+//
+//  Created by 홍세린 on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct project__________2171379_________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
